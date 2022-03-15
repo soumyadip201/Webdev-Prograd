7f21eead992cc616a407efa6631561a13076eee9
soumyadip201 <soumyadip201@gmail.com>
2022-03-15 17:19:55 +0530

    project completed

--- Assignments/Sprint-1/11_OopsBlog/script.js
+++ Assignments/Sprint-1/11_OopsBlog/script.js
@@ -1,39 +1,51 @@
-/* Fill your code*/
-document.getElementById('addBlog').addEventListener('click', function () {
-    document.getElementById('popupContact').style.display = "block";
-})
 class Blog {
-    constructor(title, detail) {
-        this.title = title;
-        this.detail = detail;
-    }
-    addTitle() {
-        var title_card = document.createElement('h1');
-        title_card.setAttribute("id", "blog-title");
-        console.log(title_card);
-        document.getElementById('card-text').appendChild(title_card);
-        title_card.innerHTML += this.title;
-
-    }
-    addDescription() {
-        var discription_card = document.createElement('p');
-        discription_card.setAttribute("id", "blog-description");
-        console.log(discription_card);
-        document.getElementById('card-text').appendChild(discription_card);
-        discription_card.innerHTML += this.detail;
-    }
+  constructor(title, details) {
+    this.title = title;
+    this.details = details;
+    this.addTitle();
+    this.addDescription();
+  }
+  //creating Heading element and that display the title of blog
+  addTitle() {
+    var title_card = document.createElement("h1");
+    title_card.setAttribute("id", "blog-tile");
+    document.getElementById("card-text").appendChild(title_card);
+    title_card.innerHTML = this.title;
+    console.log(title_card);
+  }
+  //creating paragraph element and that display the description of blog
+  addDescription() {
+    var description_card = document.createElement("p");
+    description_card.setAttribute("id", "blog-desc");
+    document.getElementById("card-text").appendChild(description_card);
+    description_card.innerHTML = this.details;
+    console.log(description_card);
+  }
 }
-
-var post = document.getElementById('post');
-post.addEventListener('click', function () {
-    var title = document.getElementById('title').value;
-    var detail = document.getElementById('detail').value;
-    var add_blog = new Blog(title, detail);
-    add_blog.addTitle();
-    add_blog.addDescription();
-    var image = document.createElement('img');
-    image.src = './assets/javascript.png';
-    var newchild = document.getElementById('flashcard')
-    newchild.insertBefore(image, newchild.childNodes[0]);
-    document.getElementById('popupContact').style.display = "none";
-})
+//display the form when clicking the button add Post
+var open1 = document.getElementById("addBlog");
+if (open1) {
+  open1.addEventListener("click", function () {
+    document.getElementById("popupContact").style.display = "block";
+  });
+}
+//close the form when clicking close button
+var exit1 = document.getElementById("close");
+if (exit1) {
+  exit1.addEventListener("click", function () {
+    document.getElementById("popupContact").style.display = "none";
+  });
+}
+//display the blog when clicking post button
+var post1 = document.getElementById("post");
+if (post1) {
+  post1.addEventListener("click", function () {
+    document.getElementById("popupContact").style.display = "none";
+    var $img = document.createElement("img");
+    $img.setAttribute("src", "./assets/java card image.svg");
+    document.getElementById("card-text").appendChild($img);
+    var $title = document.getElementById("title").value;
+    var $detail = document.getElementById("detail").value;
+    var object1 = new Blog($title, $detail);
+  });
+}
