80c7f795f7fcea38807032dcfafd91329ea7f0d8
soumyadip201 <soumyadip201@gmail.com>
2022-03-20 19:23:22 +0530

    completed project 22

---
+++ Assignments/Sprint-1/22_News-API/README.md
@@ -0,0 +1,25 @@
+![Image description](https://i1.faceprep.in/ProGrad/prograd-logo.png)
+
+# ProGrad Project-Builder | JS | LIVE NEWS
+
+## [Link to PROGRAD LIVE NEWS Github Repository](https://github.com/prograd-org/lab-js-live-news)
+
+We have learned all the important concepts in JavaScript. Now lets work with our project builder in building a smartwatch, applying all of the concepts we have just learned.
+
+### Progression 1: GET THE API
+
+Create an account in (https://newsapi.org/). Once you create an account, go to settings and get your API key.
+
+### Progression 2: INDIA NEWS-LIVE
+
+Use Axios to fetch the data from https://newsapi.org/ api and display it as shown in the output.
+
+### Progression 3: US NEWS-LIVE
+
+Show the top most news in US
+
+## OUTPUT
+
+Here is the Screenshot of the Expected Output.
+
+Happy Coding ProGrads❤️
---
+++ Assignments/Sprint-1/22_News-API/app.js
@@ -0,0 +1,99 @@
+const apikey = "b253a88b3c094841960f5c3e8cce978d";
+
+//showing dropdown menu on click
+function showdropdown() {
+  var drop = document.getElementById("country");
+  if (drop.style.visibility === "hidden") {
+    drop.style.visibility = "visible";
+  } else {
+    drop.style.visibility = "hidden";
+  }
+}
+//Showing indian news
+function india() {
+  let country = "in";
+  news(country);
+  document.getElementById("country").style.visibility = "hidden";
+}
+//showing USA news
+function us() {
+  let country = "us";
+  news(country);
+  document.getElementById("country").style.visibility = "hidden";
+}
+
+//getting news
+function news(country) {
+  // using axios to fetch data from newsapi.org
+  console.log(country);
+
+  axios
+    .get(
+      `http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apikey}`
+    )
+    .then((response) => {
+      console.log(response);
+      let section = document.querySelector("section");
+      section.innerHTML = "";
+      //checking response is not empty
+      if (response.data.articles.length) {
+        //looping on every articles
+        response.data.articles.forEach((news) => {
+          //checking if these three are avaliable
+          if (news.description && news.title && news.urlToImage) {
+            let div = document.createElement("div");
+            div.classList.add("card");
+
+            let img = document.createElement("img");
+            img.setAttribute("src", news.urlToImage);
+            img.setAttribute("alt", "Image");
+            div.appendChild(img);
+
+            let heading = document.createElement("h5");
+            let headingText = document.createTextNode(news.title);
+            heading.appendChild(headingText);
+            div.appendChild(heading);
+
+            let content = document.createElement("p");
+            let contentText = document.createTextNode(news.description);
+            content.appendChild(contentText);
+            div.appendChild(content);
+
+            let readlink = document.createElement("a");
+            let readText = document.createTextNode("Read More");
+            readlink.setAttribute("href", news.url);
+            readlink.appendChild(readText);
+            div.appendChild(readlink);
+            section.appendChild(div);
+          }
+          // if any one field is missing showing message
+          else {
+            let div = document.createElement("div");
+            div.classList.add("card");
+
+            let img = document.createElement("img");
+            img.setAttribute("src", "https://i.stack.imgur.com/WeyM8.jpg");
+            img.setAttribute("alt", "image");
+            div.appendChild(img);
+
+            let heading = document.createElement("h5");
+            let headingText = document.createTextNode("no news to show");
+            heading.appendChild(headingText);
+            div.appendChild(heading);
+
+            let content = document.createElement("p");
+            let contentText = document.createTextNode(
+              "Will update soon when avaliable"
+            );
+            content.appendChild(contentText);
+            div.appendChild(content);
+
+            section.appendChild(div);
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
